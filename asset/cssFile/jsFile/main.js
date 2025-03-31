a8b0b26684c5bf057473c9c93e13c83dfbd73e28
Sumit-sarkar <sumitopticalit89@gmail.com>
2025-03-31 14:13:58 +0600

    responsiveness complete

--- asset/cssFile/jsFile/main.js
+++ asset/cssFile/jsFile/main.js
@@ -12,3 +12,21 @@ moon.addEventListener("click", () =>{
     moon.style.background = "#000";
     document.body.classList.remove("lightmood"); //light on
 });
+
+
+// ===================================navigation menu===================
+const openMenu = document.querySelector(".openMenu");
+const closeMenu = document.querySelector(".closeMenu");
+const responsiveMenu = document.querySelector("#responsive-menubar");
+
+openMenu.addEventListener("click", () => {
+    responsiveMenu.style.top = "0";
+    openMenu.style.display = "none";
+    closeMenu.style.display = "block"
+});
+
+closeMenu.addEventListener("click", () => {
+    responsiveMenu.style.top = "-100%";
+     openMenu.style.display = "block";
+    closeMenu.style.display = "none";
+});
